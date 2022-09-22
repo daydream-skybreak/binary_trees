f7c83e8f1eff94641101c0b75b20985ca402f925
Samuel <samueldesta323@gmail.com>
2022-09-22 20:42:18 +0300

    added file 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the leaves in tree
+ * @tree: binary tree
+ * Return: number of leaves
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    size_t leaves;
+
+    if (tree == NULL || (tree->right == NULL && tree->left == NULL))
+        return (0);
+
+    leaves = binary_tree_nodes(tree->left) +
+             binary_tree_nodes(tree->right) + 1;
+
+    return (leaves);
+}
