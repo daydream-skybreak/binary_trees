a19da6cdbce194e4e5cc2be0d57b984c3503a8c6
Samuel <samueldesta323@gmail.com>
2022-09-22 13:03:59 +0300

    fixed an error in the code

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,15 +6,10 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-	if (tree == NULL)
+	size_t depth;
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
-	while (tree->parent != NULL)
-	{
-
-		depth += 1;
-		tree = tree->parent;
-	}
+	depth = binary_tree_depth(tree->parent) + 1;
 	return (depth);
 }
