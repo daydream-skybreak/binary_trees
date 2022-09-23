074b0f1e6ebf0a066b4dd82a25bbbc1001f453e7
Samuel <samueldesta323@gmail.com>
2022-09-23 21:05:46 +0300

    fixed betty style

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,13 +6,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t leaves;
+	size_t leaves;
 
-    if (tree == NULL || (tree->right == NULL && tree->left == NULL))
-        return (0);
+	if (tree == NULL || (tree->right == NULL && tree->left == NULL))
+		return (0);
 
-    leaves = binary_tree_nodes(tree->left) +
-             binary_tree_nodes(tree->right) + 1;
+	leaves = binary_tree_nodes(tree->left) +
+		 binary_tree_nodes(tree->right) + 1;
 
-    return (leaves);
+	return (leaves);
 }
